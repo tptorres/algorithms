91439d25ccded681f7f71d13a0490d837ccbb108
Ty <tptorres5@gmail.com>
2020-10-02 18:42:45 -0400

    car pooling

---
+++ LC/car_pooling.py
@@ -0,0 +1,20 @@
+# Type: Greedy, Heap
+from heapq import *
+
+
+def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+    trips.sort(key=lambda trip: trip[1])
+    minHeap = []
+    current_capacity = 0
+
+    for num, start, end in trips:
+        while minHeap and start >= minHeap[0][0]:
+            num_passengers = heappop(minHeap)[1]
+            current_capacity -= num_passengers
+
+        heappush(minHeap, (end, num))
+        current_capacity += num
+        if current_capacity > capacity:
+            return False
+
+    return True
